31557cefbb7c38e51822169227c3afba4c3aaba5
Tom Wang <me@tom.af>
2023-02-01 21:25:48 -0500

    Add running logic

--- engine/overworld/player_movement.asm
+++ engine/overworld/player_movement.asm
@@ -274,6 +274,9 @@ DoPlayerMovement::
 	call CheckIceTile
 	jr nc, .ice
 
+	call .BikeCheck
+	jr nz, .HandleWalkAndRun
+
 ; Downhill riding is slower when not moving down.
 	call .BikeCheck
 	jr nz, .walk
@@ -290,6 +293,25 @@ DoPlayerMovement::
 	call .DoStep
 	scf
 	ret
+
+.HandleWalkAndRun
+  call .CheckStandingStill
+  jr z, .shouldbewalk
+  call .CheckBHeldDown
+  jr z, .shouldbefast
+  jr .shouldbewalk
+
+.shouldbefast
+  ld a, [wPlayerState]
+  cp PLAYER_RUN
+  call nz, .StartRunning
+  jr .fast
+
+.shouldbewalk
+  ld a, [wPlayerState]
+  cp PLAYER_NORMAL
+  call nz, .StartWalking
+  jr .walk
 
 .fast
 	ld a, STEP_BIKE
@@ -732,6 +754,17 @@ ENDM
 	cp PLAYER_SKATE
 	ret
 
+.CheckBHeldDown:
+	ldh a, [hJoypadDown]
+	and B_BUTTON
+	cp B_BUTTON
+	ret
+
+.CheckStandingStill
+	ld a, [wWalkingDirection]
+	cp STANDING
+  ret
+
 .CheckWalkable:
 ; Return 0 if tile a is land. Otherwise, return carry.
 
@@ -782,6 +815,22 @@ ENDM
 	call UpdatePlayerSprite ; UpdateSprites
 	pop bc
 	ret
+
+.StartRunning:
+	push bc
+  ld a, PLAYER_RUN
+  ld [wPlayerState], a
+  call UpdatePlayerSprite
+  pop bc
+  ret
+
+.StartWalking:
+	push bc
+  ld a, PLAYER_NORMAL
+  ld [wPlayerState], a
+  call UpdatePlayerSprite
+  pop bc
+  ret
 
 CheckStandingOnIce::
 	ld a, [wPlayerTurningDirection]
